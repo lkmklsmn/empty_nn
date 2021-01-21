c1e8e59f242dc9461e3d6baf366c2b3a1bcea098
lkmklsmn <233454@mhgcp-r01.grid.bcm.edu>
2021-01-20 18:41:25 -0600

    change to sh download script

---
+++ code/download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O ./data/neurons_900_raw.h5 "http://cf.10xgenomics.com/samples/cell-exp/2.1.0/neurons_900/neurons_900_raw_gene_bc_matrices_h5.h5"
+wget -O ./data/pbmc_10k_raw.h5 "http://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_10k_v3/pbmc_10k_v3_raw_feature_bc_matrix.h5"
+wget -O ./data/kang_sampleC.h5 "https://drive.google.com/uc?export=download&id=1Z1Vxzpu17kWwZGo6f2BMKo9eLjofmdrk"
