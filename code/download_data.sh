ae6e4570bad3e9825ff084aa2761dd52c2c0ea32
Fangfang Yan <30477318+fangfang0906@users.noreply.github.com>
2021-07-15 15:28:24 -0500

    update tutorial and reproducibility file

--- code/download_data.sh
+++ code/download_data.sh
@@ -27,14 +27,25 @@ fileName=cell_hashing_original_paper.rds
 fileId=1oviPPff0-GV6t4p-eaFx5gF8fbU0-m-u
 download_large_googlefiles
 fileName=cell_hashing_retained.rds
-fileId=1G19QYFoy3AvtEIiwuo37sPrtXKDW3qIl
+fileId=114emrk75iiF9Eu4QHzk_0-gYljiVBHMa
 download_large_googlefiles
 
 ## reproduce Figure 3
-wget -O ./data/multiplexed_PBMC_results.RData "https://drive.google.com/uc?export=download&id=19x8WYyxvSjbFa1M3286NXW1y-SX2K6g7"
+wget -O ./data/multiplexed_PBMC_results.RData "https://drive.google.com/file/d/1EqpH_0qmXTPcwV29NYPcWhA0uuTIx3xb/view?usp=sharing"
 wget -O ./data/multiplexed_PBMC_demuxlet.best "https://drive.google.com/uc?export=download&id=1fVvTZE8GE3424Dp1xCqethe_8qtv5b8n"
 
 ## reproduce Figure 4
+wget -O ./data/mouse_nuclei_retained.rds "https://drive.google.com/file/d/19UhPPCy_f-wxDD-7n6DVJkpiLWCcIXPD/view?usp=sharing"
+wget -O ./data/mouse_nuclei_2k_raw.rds "https://drive.google.com/file/d/1Vh5iB_zHFhDSvGO7KG50zzLZwofYElia/view?usp=sharing"
+fileName=unspliced.csv
+fileId=15t-4EqT7RDFnjkBJiW6yzEykpAcak4pW
+download_large_googlefiles
+
+fileName=spliced.csv
+fileId=1hih3YOSummstHyVMRx9pcgZp2Bc3N-AH
+download_large_googlefiles
+
+## reproduce Figure 5
 fileName=pbmc_8k_retained.rds
 fileId=1YiYD0yt8dbkK0AC6jm9g2o85tNRsiryZ
 download_large_googlefiles
